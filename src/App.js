e4695ca3b3b3ab4f7992841a94ecdc86feba48f5
Jared Siirila <siirila@gmail.com>
2021-08-08 12:08:01 -0400

    Embedded JS in JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import * as React from 'react';
- 
+
+function getTitle(title) {
+  return title;
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello {getTitle('World')}</h1>
+
+      <label htmlFor="sarch">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
