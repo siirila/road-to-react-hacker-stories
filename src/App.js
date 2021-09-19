d2d9cf33ec8e220bea70a42b0347b119460381e1
Jared Siirila <siirila@gmail.com>
2021-09-19 16:12:56 -0400

    React State

--- src/App.js
+++ src/App.js
@@ -1,72 +1,74 @@
 import * as React from 'react';
 
-function List(props) {
-  return (
-    <ul>
-      {props.list.map(function(item) {
-        return (<Item item={item} />);
-      })}
-    </ul>
-  );
-}
-
-const Item = (props) => {
-  const item = props.item;
-
-  return (
-    <li key={item.objectID}>
-    <span>
-      <a href={item.url}>{item.title}</a>
-    </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-  </li>
-  );
-}
-
-function Search() {
-  const handleChange = (event) => {
-    console.log(event);
-  }
-
-  return (
-    <div>
-      <label htmlFor="sarch">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-    </div>
-  );
-}
-
-function App() {
-  
-const stories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
+
       <Search />
+
       <hr />
+
       <List list={stories} />
     </div>
   );
-}
- 
+};
+
+const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
+    </div>
+  );
+};
+
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
+
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
+
 export default App;
